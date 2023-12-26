b2dd3e178c72bd81b0853728c477fa649afcafb2
Ailson <robertofeitosa06@gmail.com>
2023-12-25 22:24:21 -0400

    feat: finishing website footer

--- src/views/public/site/SiteIndex.vue
+++ src/views/public/site/SiteIndex.vue
@@ -3,11 +3,13 @@
     <site-header />
     <product-section />
     <section-about />
+    <site-footer />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import SiteFooter from './components/site-footer/SiteFooter.vue'
 import SiteHeader from './components/site-header/SiteHeader.vue'
 import SectionAbout from './components/section-about/SectionAbout.vue'
 import ProductSection from './components/product-section/ProductSection.vue'
@@ -15,6 +17,7 @@ import ProductSection from './components/product-section/ProductSection.vue'
 export default defineComponent({
   name: 'SiteIndex',
   components: {
+    SiteFooter,
     SiteHeader,
     SectionAbout,
     ProductSection
---
+++ src/views/public/site/components/site-footer/SiteFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer-wrapper">
+    <p class="c4">Alguns direitos reservados</p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'SiteFooter'
+})
+</script>
+
+<style scoped>
+.footer-wrapper {
+  padding: 30px;
+  border-top: 1px solid var(--c10);
+  text-align: center;
+  background-color: var(--c12);
+}
+</style>
