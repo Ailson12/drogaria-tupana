65ee6dbba8b795620bda0941a7712169d3474b81
Ailson <robertofeitosa06@gmail.com>
2023-12-25 22:18:06 -0400

    feat: about section

--- src/views/public/site/SiteIndex.vue
+++ src/views/public/site/SiteIndex.vue
@@ -2,18 +2,21 @@
   <div>
     <site-header />
     <product-section />
+    <section-about />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import SiteHeader from './components/site-header/SiteHeader.vue'
+import SectionAbout from './components/section-about/SectionAbout.vue'
 import ProductSection from './components/product-section/ProductSection.vue'
 
 export default defineComponent({
   name: 'SiteIndex',
   components: {
     SiteHeader,
+    SectionAbout,
     ProductSection
   }
 })
---
+++ src/views/public/site/components/section-about/SectionAbout.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="about-bg">
+    <section class="about-wrapper">
+      <h1>Sobre</h1>
+      <p>
+        A Tupana se destaca das demais farmácias de Manaus por sua mistura única de remédios
+        tradicionais da Amazônia e expertise farmacêutica moderna. Nos esforçamos para oferecer uma
+        experiência de saúde verdadeiramente autêntica e holística, garantindo o bem-estar de nossa
+        comunidade em harmonia com a rica cultura Manauara e a biodiversidade da floresta amazônica.
+      </p>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'SectionAbout'
+})
+</script>
+
+<style scoped>
+.about-bg {
+  background-color: var(--c12);
+}
+
+.about-wrapper {
+  max-width: 75rem;
+  margin: 0 auto;
+  padding: 3.75rem;
+}
+
+.about-wrapper h1 {
+  font-size: 32px;
+  color: var(--c1);
+  text-align: center;
+  margin-bottom: 2.5rem;
+  font-family: 'Merriweather';
+}
+
+.about-wrapper p {
+  color: var(--c2);
+  text-align: center;
+  max-width: 60ch;
+  margin: 0 auto;
+  font-size: 20px;
+}
+</style>
